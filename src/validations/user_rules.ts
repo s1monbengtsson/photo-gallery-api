c098438c1a45a68d396f8cfb50f1b0f1ea750af3
sb <dev.simon.bengtsson@gmail.com>
2023-02-21 18:15:48 +0100

    added message for length validation

--- src/validations/user_rules.ts
+++ src/validations/user_rules.ts
@@ -6,8 +6,8 @@ import { body } from 'express-validator'
 import { getUserByEmail } from '../services/user_service'
 
 export const CreateUserRules = [
-    body('first_name').isString().bail().withMessage('First name property has to be a string').bail().trim().isLength({min: 3}),
-    body('last_name').isString().bail().withMessage('Last name property has to be a string').bail().trim().isLength({min: 3}),
+    body('first_name').isString().bail().withMessage('First name property has to be a string').bail().trim().isLength({min: 3}).withMessage('First name has to be at least 3 characters long'),
+    body('last_name').isString().bail().withMessage('Last name property has to be a string').bail().trim().isLength({min: 3}).withMessage('Last name has to be at least 3 characters long'),
     body('email').isEmail().custom(async (data: string) => {
 
 		// check if a user with that email already exists
@@ -18,5 +18,5 @@ export const CreateUserRules = [
 			return Promise.reject("Email already exists")
 		}
     }),
-	body('password').isString().withMessage('Has to be a string').bail().trim().isLength({min: 6})
+	body('password').isString().withMessage('Has to be a string').bail().trim().isLength({min: 6}).withMessage('Password has to be at least 6 characters long')
 ]
